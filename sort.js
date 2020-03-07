e7959997cb43a01fc2015bf89a74a5ad413efbb7
ally-myi <53482800+ally-myi@users.noreply.github.com>
2020-03-07 12:52:23 -0500

    Create sort.js

---
+++ sort.js
@@ -0,0 +1,14 @@
+// initializing counter and array
+var numbernames=0;
+var names = new Array();
+function SortNames() {
+  //get name from text field
+  thename=document.theform.newname.value;
+  //add to array
+  names[numbernames]=thename;
+  //increment counter
+  numbernames++;
+  //sort the array
+  names.sort();
+  document.theform.sorted.value=names.join("\n");
+  }
